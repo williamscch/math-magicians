2e6ed760697e25334d0e7f13e6843b9e81d800c5
wdavidcch <david.13.2121@gmail.com>
2022-08-02 16:31:53 -0400

    Add App Component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-useless-constructor */
+import React from 'react';
+import CalculatorDisplay from './Calculator';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <CalculatorDisplay />
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CalculatorDisplay from './components/Calculator';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CalculatorDisplay />
+    <App />
   </React.StrictMode>,
 );
