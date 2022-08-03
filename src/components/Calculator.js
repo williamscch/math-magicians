40efb8f0415b4a179f1a48846f13704897c93563
wdavidcch <david.13.2121@gmail.com>
2022-08-03 16:41:14 -0400

    Add handleClick function, and style

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class CalculatorDisplay extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const answer = calculate(this.state, e.target.textContent);
+    this.setState(answer);
+  }
 
   render() {
+    const { total, next } = this.state;
     return (
       <div className="calculatorContainer">
         <div className="calculator">
-          <h2 className="showNumbers">0</h2>
+          <h2 className="showNumbers">{next || total}</h2>
           <div className="line line1">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="operator">/</button>
+            <button type="button" className="operator" onClick={this.handleClick}>AC</button>
+            <button type="button" className="operator" onClick={this.handleClick}>+/-</button>
+            <button type="button" className="operator" onClick={this.handleClick}>%</button>
+            <button type="button" className="operator" onClick={this.handleClick}>÷</button>
           </div>
           <div className="line line2">
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="operator">x</button>
+            <button type="button" onClick={this.handleClick}>7</button>
+            <button type="button" onClick={this.handleClick}>8</button>
+            <button type="button" onClick={this.handleClick}>9</button>
+            <button type="button" onClick={this.handleClick} className="operator">x</button>
           </div>
           <div className="line line3">
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="operator">-</button>
+            <button type="button" onClick={this.handleClick}>4</button>
+            <button type="button" onClick={this.handleClick}>5</button>
+            <button type="button" onClick={this.handleClick}>6</button>
+            <button type="button" onClick={this.handleClick} className="operator">-</button>
           </div>
           <div className="line line4">
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="operator">+</button>
+            <button type="button" onClick={this.handleClick}>1</button>
+            <button type="button" onClick={this.handleClick}>2</button>
+            <button type="button" onClick={this.handleClick}>3</button>
+            <button type="button" onClick={this.handleClick} className="operator">+</button>
           </div>
           <div className="line line5">
-            <button type="button">0</button>
-            <button type="button">.</button>
-            <button type="button" className="operator">=</button>
+            <button type="button" onClick={this.handleClick}>0</button>
+            <button type="button" onClick={this.handleClick}>.</button>
+            <button type="button" onClick={this.handleClick} className="operator">=</button>
           </div>
         </div>
       </div>
